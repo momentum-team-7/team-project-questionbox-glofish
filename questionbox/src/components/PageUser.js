6dcea6d45fa1bdfb590065795c06c031d43952b8
arthurstuart <arthur.stuart10@gmail.com>
2021-04-07 17:12:44 -0400

    Answers rendering

--- questionbox/src/components/PageUser.js
+++ questionbox/src/components/PageUser.js
@@ -1,29 +1,40 @@
 import React, { useEffect, useState } from "react";
 import { getUserProfile } from "../userApis.js";
-import { Link, useParams } from 'react-router-dom';
+import { Link, useParams } from "react-router-dom";
 import QuestionDetail from "./QuestionDetail.js";
 
-export default function PageUser({userProfiles, id}) {
-    const [userProfile, setUserProfile] = useState({});
-    const { profile } = useParams();
-console.log(id)
-    useEffect(() => {
-        
-        setUserProfile(getUserProfile(id)) 
-}, [id])
-console.log(userProfile.questions)
-    return (
-    <>
-        <h1 className="user-profile">Your Profile</h1>
-            {userProfiles && userProfiles.map((userProfile, id) => 
-            <li key={userProfile.id}>                             
-            <p>Member since:{userProfile.date_created}</p>
-            <h2>Responses Given</h2>
-            <QuestionDetail answers={userProfile.question.answers} />
-            <Link to='/'>Go Back</Link>
-            </li>   
-            )}
-</>
+export default function PageUser({ id }) {
+  const [userProfile, setUserProfile] = useState({});
+  const { profile } = useParams();
+  console.log(id);
+  useEffect(() => {
+    setUserProfile(getUserProfile(id));
+  }, [id]);
 
-)
+  // {
+  //     answers: array,
+  //     id: number,
+  //     questions: [{}],
+  //     username: string
+  // }
+
+  //Missing date: date_created
+
+  console.log(userProfile);
+  return userProfile ? (
+    <>
+      <h1 className="user-profile">Your Profile</h1>
+      <li key={userProfile.id}>
+        {/* Commment back in once date_created is resolved */}
+        {/* <p>Member since:{userProfile.date_created}</p> */}
+        {!!userProfile.questions &&
+          userProfile.questions.map((question) => (
+            <QuestionDetail question={question} />
+          ))}
+        <Link to="/">Go Back</Link>
+      </li>
+    </>
+  ) : (
+    <h2>Loading</h2>
+  );
 }
