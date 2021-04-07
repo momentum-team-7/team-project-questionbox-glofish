0a1ec9f6da1ca97bb53eea81a710e4bdcbb87966
arthurstuart <arthur.stuart10@gmail.com>
2021-04-07 15:30:39 -0400

    where do I go from here

--- questionbox/src/components/PageUser.js
+++ questionbox/src/components/PageUser.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { getUserProfile } from "../userApis.js";
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import QuestionDetail from "./QuestionDetail.js";
 
 export default function PageUser({userProfiles, id}) {
     const [userProfile, setUserProfile] = useState({});
+    const { profile } = useParams();
 console.log(id)
     useEffect(() => {
         
@@ -12,14 +13,17 @@ console.log(id)
 }, [id])
 console.log(userProfile.questions)
     return (
+    <>
         <h1 className="user-profile">Your Profile</h1>
-            {userProfiles && userProfiles.map((userProfile, id) => (
+            {userProfiles && userProfiles.map((userProfile, id) => 
             <li key={userProfile.id}>                             
             <p>Member since:{userProfile.date_created}</p>
             <h2>Responses Given</h2>
-            <QuestionDetail answers={question.answers} />
+            <QuestionDetail answers={userProfile.question.answers} />
             <Link to='/'>Go Back</Link>
             </li>   
-            ))}
-    )
+            )}
+</>
+
+)
 }
