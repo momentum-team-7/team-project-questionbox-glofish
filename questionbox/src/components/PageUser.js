e83d69c9b6ae78ddb8e6bec87f1ca4d711795bbe
arthurstuart <arthur.stuart10@gmail.com>
2021-04-07 09:09:02 -0400

    Not quite there but close

--- questionbox/src/components/PageUser.js
+++ questionbox/src/components/PageUser.js
@@ -3,24 +3,24 @@ import { getUserProfile } from "../userApis.js";
 import { Link } from 'react-router-dom';
 import AnswerList from "./Answerlist.js";
 
-export default function PageUser({id}) {
-const [userProfile, setUserProfile] = useState({})
+export default function PageUser({userProfiles, id}) {
+    const [userProfile, setUserProfile] = useState({});
 console.log(id)
     useEffect(() => {
         
         setUserProfile(getUserProfile(id)) 
 }, [id])
 console.log(userProfile.questions)
-return (
-    <div className='User-profile'>
-        <h1>PageUser</h1>
-        <p>{userProfile.id}</p>
-      <p>{userProfile.username}</p>
-        <p>{userProfile.date_created}</p>
-        {/* {userProfile.questions.map(question =>
-            <AnswerList answers={question.answers}/>
-            )} */}
-        <Link to='/'>Go Back</Link>
-    </div>
-)
+    return (
+            <div>
+                {userProfiles && userProfiles.map((userProfile) => (
+                    <li key={userProfile.id}>                             
+                    <p>Member since: {userProfile.date_created}</p>
+                    <h2 className="questions-answers">{userProfile.username} User Responses</h2>
+                    <AnswerList answers={userProfile.answers}/>
+                    <Link to='/'>Go Back</Link>
+                    </li>
+                ))}
+            </div>
+    )
 }
